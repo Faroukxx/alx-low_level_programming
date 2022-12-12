ab37b41189e83cea675c3aa4e9ed222b8ce38a44
Faroukxx <faroukabdullahi98@gmail.com>
2022-12-12 13:30:45 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,5 +19,9 @@ int main(void)
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
+	else if (last > 6 || last != 0)
+{
+	printf("Last digit of %d is %d and is Less than 6 and not 0", n, last);
+}
 	return (0);
 }

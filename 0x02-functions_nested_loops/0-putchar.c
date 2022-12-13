4939fd9ae4a09fd62b4aad080351886f9937cb55
Faroukxx <faroukabdullahi98@gmail.com>
2022-12-13 13:53:50 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
+	_putchar("_putchar\n");
 	return (0);
 }
